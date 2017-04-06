354c2590b2644e54b6dd1add83f2c6713d2c4acf
polzoom <polzoom@mail.ru>
2017-04-07 00:53:47 +0300

    draw table

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  $.ajax({
+    type: 'GET',
+    url: 'https://gyqv2q06tb.execute-api.eu-central-1.amazonaws.com/development/events',
+    success: events => {
+      $('#table').append('<table class="table-bordered"/>');
+  
+      events.forEach(event => {
+        const fields = event.fields;
+        const fieldsCounter = Object.keys(fields).length;
+        
+        if (fieldsCounter == 8) {
+          const tr = $('<tr/>');
+          for (let prop in fields) {
+            tr.append(`<td>${fields[prop]}</td>`);
+          }
+          $('#table table').append(tr);
+        }
+      });
+    }
+  });
+});
