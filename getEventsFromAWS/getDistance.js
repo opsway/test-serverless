1c9369382235557aac29e7280dbc607e94268c25
polzoom <polzoom@mail.ru>
2017-04-06 19:06:57 +0300

    init

--- getEventsFromAWS/getDistance.js
+++
@@ -1,9 +0,0 @@
-const Distance = require('geo-distance');
-
-module.exports = (lat, lon, geoPositionString) => {
-  const parsedData = geoPositionString.split(',');
-  const parsedLat = parseFloat(parsedData[0]);
-  const parsedLon = parseFloat(parsedData[1]);
-  
-  return Distance.between({ lat, lon }, { lat: parsedLat, lon: parsedLon });
-};
--- index.js
+++
@@ -1,9 +0,0 @@
-const getEvents = require('./getEventsFromAWS/exports').handler;
-
-getEvents({
-  queryStringParameters: {
-    lat: 59.975007,
-    lon: 30.284635,
-    searchRadius: 11000,
-  }
-}, {}, (err, v) => console.log('err ', err, 'v ', v));
