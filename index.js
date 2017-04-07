6f32e8473b58c8bf8ea231265a7def5f54bb260b
polzoom <polzoom@mail.ru>
2017-04-07 13:02:01 +0300

    fix styles

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-$(document).ready(() => {
+const fields = [
+'#',
+'Название',
+'Описание',
+'Дата создания',
+'Место',
+'Координаты',
+'Сайт',
+];
+
+var x = document.getElementById("position");
+
+const showPosition = position => {
+  x.innerHTML = "Latitude: " + position.coords.latitude.toFixed(6) +
+    "<br> Longitude: " + position.coords.longitude.toFixed(6);
+};
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+const renderTable = (params) => {
   $.ajax({
     type: 'GET',
     url: 'https://gyqv2q06tb.execute-api.eu-central-1.amazonaws.com/development/events',
+    data: params,
     success: events => {
-      console.log('success response: ', events);
-      $('#table').append('<table class="table-bordered"/>');
-  
+      $('#app').append('<table class="table-bordered" id="table">');
+      
+      const thead = $('<thead>')
+      const tr = $('<tr class="header">');
+      fields.forEach(field => {
+        const td = $(`<td>${field}</td>`);
+        tr.append(td);
+      });
+      thead.append(tr);
+      $('#app table').append(thead);
+      
+      const tbody = $('<tbody>');
       events.forEach(event => {
         const fields = event.fields;
         const fieldsCounter = Object.keys(fields).length;
@@ -13,11 +48,31 @@ $(document).ready(() => {
         if (fieldsCounter == 8) {
           const tr = $('<tr/>');
           for (let prop in fields) {
-            tr.append(`<td>${fields[prop]}</td>`);
+            if (prop !== 'Category') {
+              const td = $('<td>').text(fields[prop]);
+              tr.append(td);
+            }
           }
-          $('#table table').append(tr);
+          tbody.append(tr);
         }
       });
+      
+      $('#app table').append(tbody);
     }
   });
-});
+};
+
+const search = () => {
+  $('#app')[0].innerHTML = '';
+  navigator.geolocation.getCurrentPosition(position => {
+    const lat = position.coords.latitude.toFixed(6);
+    const lon = position.coords.longitude.toFixed(6);
+    const searchRadius = $('#r')[0].value;
+    if (lat && lon && searchRadius) {
+      renderTable({ lat, lon, searchRadius });
+    }
+  });
+};
+
+getLocation();
+renderTable();
