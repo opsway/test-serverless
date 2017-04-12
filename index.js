23d724bd7c385d2cf36c4804dfae7911186820f0
polzoom <polzoom@mail.ru>
2017-04-12 15:09:39 +0300

    change parameters for test

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ getEvents({
   queryStringParameters: {
     lat: 50.461001,
     lon: 30.351268,
-    searchRadius: 100,
-    category: 'recIBk1CLEFOYNrc6',
+    searchRadius: 100000,
+    // category: 'recIBk1CLEFOYNrc6',
   }
-}, {}, (err, v) => console.log('err ', err, 'v ', v));
+}, {
+  
+}, (err, v) => console.log('Error: ', err, 'v ', v));
